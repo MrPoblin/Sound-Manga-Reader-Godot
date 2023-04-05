7e8fe0b336bd2d40a9d85a6e396621f9530a67f9
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2023-04-05 20:58:18 +0300

    Manga_List -> MangaList

--- Scripts/UI/MangaButtonTemplate.gd
+++ Scripts/UI/MangaButtonTemplate.gd
@@ -8,5 +8,5 @@ func _on_TemplateButton_pressed():
 	State.Core = Dict
 	State.FolderLocation = location
 	var root_node = get_tree().get_root()
-	var ChapterNode = root_node.get_node("Main/Menu/Manga_List/ChapterSelect")
+	var ChapterNode = root_node.get_node("Main/Menu/MangaList/ChapterSelect")
 	ChapterNode.visible = true
