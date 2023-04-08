a3a2bebbe352ca3b349fe6d6c965fe08893545da
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2023-04-08 20:19:48 +0300

    Make load_title() global

--- Scripts/State.gd
+++ Scripts/State.gd
@@ -3,3 +3,11 @@ extends Node
 var Core:Dictionary
 var FolderLocation:String
 
+func load_title(file_path, folder_path):
+	var file = File.new()
+	file.open(file_path, file.READ)
+	var text = file.get_as_text()
+	var parse = JSON.parse(text)
+	var dict = parse.result
+	file.close()
+	return dict
--- Scripts/UI/MangaList.gd
+++ Scripts/UI/MangaList.gd
@@ -1,10 +1,10 @@
 extends VBoxContainer
 
 onready var TemplateButton = load("res://Scenes/UI/MangaButtonTemplate.tscn")
+onready var folder_path :String = Config.config.get_value("storage", "manga")
 
 func _ready():
-	var manga_path :String = Config.config.get_value("storage", "manga")
-	check_all_folders(manga_path)
+	check_all_folders(folder_path)
 	if(self.get_child_count() <= 1):
 		$no_manga.visible = true
 
@@ -27,19 +27,10 @@ func find_json(folder_path):
 		var file_path = dir.get_next()
 		while file_path != "":
 			if(file_path == "script.json"):
-				load_title(folder_path+"/"+file_path, folder_path)
-				#print(json_dict["title"])
+				var dict = State.load_title(folder_path+"/"+file_path, folder_path)
+				add_button(dict["title"], dict, folder_path)
 			file_path = dir.get_next()
 		dir.list_dir_end()
-
-func load_title(file_path, folder_path):
-	var file = File.new()
-	file.open(file_path, file.READ)
-	var text = file.get_as_text()
-	var parse = JSON.parse(text)
-	var dix = parse.result
-	file.close()
-	add_button(dix["title"], dix, folder_path)
 
 func add_button(title, dict, folder_path):
 	$no_manga.visible = false
