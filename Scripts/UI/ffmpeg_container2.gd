d4f202ea67bb200c79228b26f8d8414e2874b383
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2023-04-25 23:04:31 +0300

    Convert Dialog designed

--- Scripts/UI/ffmpeg_container2.gd
+++ Scripts/UI/ffmpeg_container2.gd
@@ -6,7 +6,11 @@ func _ready():
 	$op_ffmpeg.add_item ( "Copy", 1 )
 	$op_ffmpeg.add_item ( "Replace", 2 )
 	$b_ffmpeg_convert/convert_popup.get_ok().visible = false
-	
+	$b_ffmpeg_convert/convert_popup.add_button("Replace", true, "replace")
+	$b_ffmpeg_convert/convert_popup.add_button("Copy", false, "copy")
 
 func _on_b_ffmpeg_convert_pressed():
 	$b_ffmpeg_convert/convert_popup.popup_centered()
+
+func _on_convert_popup_custom_action(action):
+	$b_ffmpeg_convert/convert_popup.hide()
