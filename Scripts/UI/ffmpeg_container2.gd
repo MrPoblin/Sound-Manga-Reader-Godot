a05ac2ff2a91c3727cc8c9883f4e9f2a15f06bd1
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2023-04-25 23:38:49 +0300

    Check ffmpeg on convert

--- Scripts/UI/ffmpeg_container2.gd
+++ Scripts/UI/ffmpeg_container2.gd
@@ -1,5 +1,5 @@
 extends HBoxContainer
-
+ 
 
 func _ready():
 	$op_ffmpeg.add_item ( "Disabled", 0 )
@@ -9,8 +9,24 @@ func _ready():
 	$b_ffmpeg_convert/convert_popup.add_button("Replace", true, "replace")
 	$b_ffmpeg_convert/convert_popup.add_button("Copy", false, "copy")
 
-func _on_b_ffmpeg_convert_pressed():
+func _on_b_ffmpeg_convert_pressed() -> void:
 	$b_ffmpeg_convert/convert_popup.popup_centered()
 
-func _on_convert_popup_custom_action(action):
+func _on_convert_popup_custom_action(action) -> void:
 	$b_ffmpeg_convert/convert_popup.hide()
+	var mangaPath: String = Config.config.get_value("storage", "manga", OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS) + "/")
+	var musicPath: String = Config.config.get_value("storage", "music", OS.get_system_dir(OS.SYSTEM_DIR_DOWNLOADS) + "/")
+	var ffmpegPath: String
+	if(Config.checkFFmpeg($"%le_ffmpeg".text)):
+		ffmpegPath = $"%le_ffmpeg".text
+	elif(Config.checkFFmpeg(Config.config.get_value("storage", "ffmpeg", ""))):
+		ffmpegPath = Config.config.get_value("storage", "ffmpeg", "")
+	else: 
+		$ffmpeg_error_popup.popup_centered()
+		return
+	
+	match action:
+		"copy":
+			pass
+		"replace":
+			pass
